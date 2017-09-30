2937c39b1d5b83be680149370de04d7983a5bb03
vakemi <valquiria.akemi@gmail.com>
2017-09-30 15:53:14 +0000

    Teste

---
+++ DemoCore/DemoCore/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Route("Valquiria/teste/{teste}")]
+        public IActionResult Index()
+        {
+            //return "Bateu no Home/Index";
+
+            return View();
+
+        }
+
+    }
+}
